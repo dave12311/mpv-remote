e37a7b036e047edf4ae6486579921549f10d2c07
Dave12311 <horvath.david987@gmail.com>
2022-06-29 15:37:28 +0200

    Finalized player view layout

--- client/mpv_remote/lib/player_view/player_view.dart
+++ client/mpv_remote/lib/player_view/player_view.dart
@@ -14,56 +14,74 @@ class _PlayerViewState extends State<PlayerView> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onVerticalDragEnd: (details) {
-        if (details.primaryVelocity! > 0) {
-          Navigator.pop(context);
-        }
-      },
-      child: Container(
-        color: const Color(0xff2c3e50),
-        child: Hero(
-          tag: 'floating_player',
-          child: Material(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 8, left: 8, right: 8),
-              child: Container(
-                decoration: const BoxDecoration(
-                    color: Color(0xff496684),
-                    borderRadius:
-                        BorderRadius.vertical(top: Radius.circular(30))),
-                child: Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Wrap(
-                      alignment: WrapAlignment.center,
-                      children: [
-                        const Text('Raised by Wolves S01E05',
+    return Container(
+      color: const Color(0xff2c3e50),
+      child: Hero(
+        tag: 'floating_player',
+        child: Material(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 8, left: 8, right: 8),
+            child: Container(
+              decoration: const BoxDecoration(
+                  color: Color(0xff496684),
+                  borderRadius:
+                      BorderRadius.vertical(top: Radius.circular(30))),
+              child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(bottom: 100),
+                        child: Text('Raised by Wolves S01E05',
                             textAlign: TextAlign.center,
                             maxLines: 3,
                             softWrap: true,
                             style: TextStyle(
                                 fontSize: 20, fontWeight: FontWeight.bold)),
-                        Column(
-                          children: [
-                            Slider(
-                              max: _length.toDouble(),
-                              value: _time.toDouble(),
-                              onChanged: (value) {
-                                setState(() {
-                                  _time = value;
-                                });
-                              },
+                      ),
+                      
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 30),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: const [
+                                IconButton(
+                                    onPressed: null,
+                                    iconSize: 30,
+                                    icon: Icon(Icons.fast_rewind)),
+                                IconButton(
+                                    onPressed: null,
+                                    iconSize: 30,
+                                    icon: Icon(Icons.fast_forward)),
+                              ],
                             ),
-                            Row(
+                          ),
+                          Slider(
+                            max: _length.toDouble(),
+                            value: _time.toDouble(),
+                            onChanged: (value) {
+                              setState(() {
+                                _time = value;
+                              });
+                            },
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 30),
+                            child: Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: const [
                                 Text('0:00'),
                                 Text('30:00'),
                               ],
-                            )
-                          ],
-                        ),
-                        Row(
+                            ),
+                          ),
+                        ],
+                      ),
+                      Expanded(
+                        child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: const [
                             IconButton(
@@ -72,7 +90,7 @@ class _PlayerViewState extends State<PlayerView> {
                                 icon: Icon(Icons.skip_previous)),
                             IconButton(
                                 onPressed: null,
-                                iconSize: 80,
+                                iconSize: 90,
                                 icon: Icon(Icons.pause)),
                             IconButton(
                                 onPressed: null,
@@ -80,7 +98,10 @@ class _PlayerViewState extends State<PlayerView> {
                                 icon: Icon(Icons.skip_next)),
                           ],
                         ),
-                        Row(
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 25),
+                        child: Row(
                           children: [
                             const IconButton(
                                 onPressed: null,
@@ -102,25 +123,25 @@ class _PlayerViewState extends State<PlayerView> {
                                 icon: Icon(Icons.volume_up)),
                           ],
                         ),
-                        Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: const [
-                              IconButton(
-                                  onPressed: null,
-                                  iconSize: 30,
-                                  icon: Icon(Icons.subtitles)),
-                              IconButton(
-                                  onPressed: null,
-                                  iconSize: 30,
-                                  icon: Icon(Icons.crop_free)),
-                              IconButton(
-                                  onPressed: null,
-                                  iconSize: 30,
-                                  icon: Icon(Icons.tune)),
-                            ]),
-                      ],
-                    )),
-              ),
+                      ),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: const [
+                            IconButton(
+                                onPressed: null,
+                                iconSize: 30,
+                                icon: Icon(Icons.subtitles)),
+                            IconButton(
+                                onPressed: null,
+                                iconSize: 30,
+                                icon: Icon(Icons.crop_free)),
+                            IconButton(
+                                onPressed: null,
+                                iconSize: 30,
+                                icon: Icon(Icons.tune)),
+                          ]),
+                    ],
+                  )),
             ),
           ),
         ),
